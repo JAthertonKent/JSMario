97df5f1947dd2373cfba93b062802710d938447a
James Atherton Kent <jkent@thoughtworks.com>
2013-08-30 15:20:45 -0700

    magic

---
+++ js/spec/EntitySpec.js
@@ -0,0 +1,49 @@
+describe("Entity2d", function () {
+    var background;
+    var entity;
+    var position;
+    var mario_sprite;
+    var background_sprite;
+
+
+    beforeEach(function () {
+        position = jasmine.createSpyObj('vector2d', ['getX', 'getY', 'addX', 'addY']);
+        mario_sprite = jasmine.createSpyObj('sprite', ['draw']);
+        background_sprite = jasmine.createSpyObj('sprite', ['draw']);
+        mario = new Entity2d(position, mario_sprite);
+        background = new Entity2d(position, background_sprite);
+    });
+
+    it("should draw its sprite", function () {
+        mario.draw();
+        expect(mario_sprite.draw).toHaveBeenCalledWith(position);
+    });
+
+    it("should draw background", function(){
+        background.draw();
+        expect(background_sprite.draw).toHaveBeenCalledWith(position);
+    });
+
+    it("should not let mario go underground", function(){
+        var local_mario = new Entity2d(new Vector2d(200, 100), mario_sprite);
+        local_mario.draw();
+        for(var i = 0; i < 100; i++){
+            local_mario.moveDown();
+        }
+        expect(local_mario.getY()).toBeLessThan(401);
+    });
+
+    it("should not let mario get too high", function(){
+        var local_mario = new Entity2d(new Vector2d(200, 100), mario_sprite);
+        local_mario.draw();
+        for(var i = 0; i < 100; i++){
+            local_mario.moveUp();
+        }
+        expect(local_mario.getY()).toBeGreaterThan(-1);
+    });
+
+    it("should come back down to earth", function() {
+        mario.draw();
+        expect(mario.gravity).toHaveBeenCalled();
+    })
+});
---
+++ js/src/entity2d.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function Entity2d(position, sprite) {
+    this.position = position;
+    this.sprite = sprite;
+    this.step = 5;
+}
+
+Entity2d.prototype.draw = function() {
+    this.sprite.draw(this.position);
+};
+
+Entity2d.prototype.getX = function() {
+    return this.position.getX();
+};
+
+Entity2d.prototype.getY = function() {
+    return this.position.getY();
+};
+
+Entity2d.prototype.moveLeft = function() {
+    return this.position.addX(-this.step);
+};
+
+Entity2d.prototype.moveRight = function() {
+    return this.position.addX(this.step);
+};
+
+Entity2d.prototype.moveUp = function() {
+    var bound = 0;
+    return this.position.getY() < bound ? this.position.addY(-this.position.getY()) : this.position.addY(-this.step);
+};
+
+Entity2d.prototype.moveDown = function() {
+    var bound = 400;
+    return this.position.getY() > bound ? this.position.addY(-(this.position.getY()-bound)) : this.position.addY(this.step);
+};
+
