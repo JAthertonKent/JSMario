2fc970c6aefd28c9fdefea6e9338423e92a31ffb
James Atherton Kent <james.atherton.kent@gmail.com>
2013-09-01 01:05:15 -0700

    Refactored: background tests are now in their own describe block

--- js/spec/EntitySpec.js
+++ js/spec/EntitySpec.js
@@ -28,11 +28,7 @@ describe("Entity2d", function () {
         expect(mario_sprite.draw).toHaveBeenCalledWith(position);
     });
 
-    it("should draw background", function() {
-        background.draw();
-        expect(background_sprite.draw).toHaveBeenCalledWith(position);
-    });
-
+  
     it("should not let mario go underground", function() {
         var local_mario = new Entity2d(new Vector2d(200, 100), mario_sprite);
         local_mario.draw();
@@ -50,39 +46,49 @@ describe("Entity2d", function () {
         }
         expect(local_mario.getY()).toBeGreaterThan(-1);
     });
-
-    it("should scroll two backgrounds at once", function() {
-        scene.drawScene();
-        scene.keypress({which: 39});
-        expect(spyBackground.position.addX).toHaveBeenCalled();
-        expect(spyBackground2.position.addX).toHaveBeenCalled();
-    });
-
-   it("should move a background to the far right when it is out view", function() {
-        background = new Background(new Vector2d(-795, 0), background_sprite);
-        background.moveLeft();
-        expect(background.getX()).toBeGreaterThan(799);
-    });
-
-   it("should let mario move to left most side of the scene", function() {
+ 
+    it("should let mario move to left most side of the scene", function() {
         spyMario = jasmine.createSpyObj('entity2d', ['moveLeft', 'draw']);
         scene = new Scene([spyBackground, spyBackground2], spyMario);
         scene.drawScene();
         scene.keypress({which: 37});
         expect(spyMario.moveLeft).toHaveBeenCalled();
-   });
+    });
 
-   it("should prevent mario from going off screen when he goes left", function() {
+    it("should prevent mario from going off screen when he goes left", function() {
         mario = new Entity2d(new Vector2d(5, 200), mario_sprite);
         mario.moveLeft();
         expect(mario.getX()).toBeGreaterThan(0);    
-   });
+    });
 
-   it("should let mario walk around half the scene without the background moving", function() {
+    it("should let mario walk around half the scene", function() {
        mario = new Entity2d(new Vector2d(5, 200), mario_sprite);
        scene = new Scene([spyBackground, spyBackground2], mario);
        scene.drawScene();
        scene.keypress({which: 39});
        expect(mario.getX()).toEqual(10);   
-   });
+    });
+
+    describe("Background", function () {
+
+        it("should draw background", function() {
+            background.draw();
+            expect(background_sprite.draw).toHaveBeenCalledWith(position);
+        });
+
+        it("should move a background to the far right when it is out view", function() {
+            background = new Background(new Vector2d(-795, 0), background_sprite);
+            background.moveLeft();
+            expect(background.getX()).toBeGreaterThan(799);
+        });
+
+        it("should scroll two backgrounds at once", function() {
+            scene.drawScene();
+            scene.keypress({which: 39});
+            expect(spyBackground.position.addX).toHaveBeenCalled();
+            expect(spyBackground2.position.addX).toHaveBeenCalled();
+        });
+
+    });
+
 });
