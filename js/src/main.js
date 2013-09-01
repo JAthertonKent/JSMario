e164a69a480e84a8d31670e5300ec71f0095a1d4
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-08-31 19:35:29 -0700

    minor clean up/refactoring

--- js/src/main.js
+++ js/src/main.js
@@ -4,23 +4,26 @@ var game = game || {};
 
 game.canvas = document.getElementById('gameOne');
 game.context = game.canvas.getContext('2d');
+
 var background = new Entity2d(new Vector2d(0, 0), new Sprite(game.context, 'img/bg.jpg'));
 var secondBackground = new Entity2d(new Vector2d(800, 0), new Sprite(game.context, 'img/bg.jpg'));
 var mario = new Entity2d(new Vector2d(200, 100), new Sprite(game.context, 'img/mario.gif'));
 
 game.scene = new Scene([background, secondBackground], mario);
 
-(function (window) {
+$(document.body).on('keydown', function (event) {
+    game.scene.keypress(event);
+});
+
+var start = function (window) {
     
     function gameLoop() {
         game.context.clearRect(0, 0, 800, 450);
         game.scene.drawScene();
     }
-
+    
     window.setInterval(gameLoop, 1000 / 60); // 60fps
+};
 
-}(window));
 
-$(document.body).on('keydown', function (event) {
-    game.scene.keypress(event);
-});
+start(window);
