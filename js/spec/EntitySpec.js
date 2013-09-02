3351423d2b75777893fac8113768fc4d99978c42
James Atherton Kent <james.atherton.kent@gmail.com>
2013-09-01 20:37:45 -0700

    split up sec into multiple describe blocks

--- js/spec/EntitySpec.js
+++ js/spec/EntitySpec.js
@@ -25,70 +25,55 @@ describe("Entity2d", function () {
         scene = new Scene([spyBackground, spyBackground2], mario);
     });
 
-    it("should draw its sprite", function () {
-        mario.draw();
-        expect(mario_sprite.draw).toHaveBeenCalledWith(position);
+    describe("Actor", function () {
+       
+        it("should draw its sprite", function () {
+            mario.draw();
+            expect(mario_sprite.draw).toHaveBeenCalledWith(position);
+        });
+    
+        it("should not go underground", function() {
+            mario = new Actor(new Vector2d(200, 100), mario_sprite);
+            mario.draw();
+            for(var i = 0; i < 100; i++){
+                mario.moveDown();
+            }
+            expect(mario.getY()).toBeLessThan(401);
+        });
+
+        it("should not get too high", function(){
+            mario = new Actor(new Vector2d(200, 100), mario_sprite);
+            mario.draw();
+            for(var i = 0; i < 100; i++){
+                mario.moveUp();
+            }
+            expect(mario.getY()).toBeGreaterThan(-1);
+        });
+    
+        it("should flip when walks left", function(){
+            scene.drawScene();
+            scene.keypress({which: 37});
+            expect(mario_sprite.flipImage).toHaveBeenCalled();
+        });
     });
+    
   
-    it("should not let mario go underground", function() {
-        mario = new Actor(new Vector2d(200, 100), mario_sprite);
-        mario.draw();
-        for(var i = 0; i < 100; i++){
-            mario.moveDown();
-        }
-        expect(mario.getY()).toBeLessThan(401);
-    });
-
-    it("should not let mario get too high", function(){
-        mario = new Actor(new Vector2d(200, 100), mario_sprite);
-        mario.draw();
-        for(var i = 0; i < 100; i++){
-            mario.moveUp();
-        }
-        expect(mario.getY()).toBeGreaterThan(-1);
-    });
- 
-    it("should let mario move to left most side of the scene", function() {
-        scene = new Scene([spyBackground, spyBackground2], spyMario);
-        scene.drawScene();
-        scene.keypress({which: 37});
-        expect(spyMario.moveLeft).toHaveBeenCalled();
-    });
-
-    it("should prevent mario from going off screen when he goes left", function() {
-        mario = new Actor(new Vector2d(5, 200), mario_sprite);
-        mario.moveLeft();
-        expect(mario.getX()).toBeGreaterThan(0);    
-    });
-
-    it("should let mario walk around half the scene", function() {
-       mario = new Actor(new Vector2d(5, 200), mario_sprite);
-       scene = new Scene([spyBackground, spyBackground2], mario);
-       scene.drawScene();
-       scene.keypress({which: 39});
-       expect(mario.getX()).toEqual(10);   
-    });
-
-    it("should flip mario when he walks left", function(){
-        scene.drawScene();
-        scene.keypress({which: 37});
-        expect(mario_sprite.flipImage).toHaveBeenCalled();
-    });
-
     describe("Background", function () {
 
         it("should draw background", function() {
+            background = new Background(position, background_sprite);
             background.draw();
             expect(background_sprite.draw).toHaveBeenCalledWith(position);
         });
 
-        it("should move a background to the far right when it is out view", function() {
+        it("should rotate back into view", function() {
             background = new Background(new Vector2d(-795, 0), background_sprite);
             background.moveLeft();
             expect(background.getX()).toBeGreaterThan(799);
         });
 
-        it("should scroll two backgrounds at once", function() {
+        it("should scroll two images at once", function() {
+            scene = new Scene([spyBackground, spyBackground2], mario);
             scene.drawScene();
             scene.keypress({which: 39});
             expect(spyBackground.position.addX).toHaveBeenCalled();
---
+++ js/spec/SceneSpec.js
@@ -0,0 +1,29 @@
+describe("Scene", function() {
+
+    beforeEach(function () {
+        spyMario = jasmine.createSpyObj('actor', ['moveLeft', 'moveRight', 'draw']);
+        spyBackground = jasmine.createSpyObj('background', ['moveRight']);
+        spyBackground2 = jasmine.createSpyObj('background', ['moveRight']);
+        background_sprite = jasmine.createSpyObj('sprite', ['draw']);
+        mario_sprite = jasmine.createSpyObj('sprite', ['draw']);
+        spyBackground.sprite = background_sprite;
+        spyBackground2.sprite = background_sprite;
+    });
+
+
+    it("should prevent the actor from escaping by the left most side", function() {
+        scene = new Scene([spyBackground, spyBackground2], spyMario);
+        scene.drawScene();
+        scene.keypress({which: 37});
+        expect(spyMario.moveLeft).toHaveBeenCalled();
+    });
+
+    it("should let the actor walk around half the scene", function() {
+        mario = new Actor(new Vector2d(5, 200), mario_sprite);
+        scene = new Scene([spyBackground, spyBackground2], mario);
+        scene.drawScene();
+        scene.keypress({which: 39});
+        expect(mario.getX()).toEqual(10);   
+    });
+
+});
