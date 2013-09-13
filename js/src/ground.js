2df9ae1034bfa0a1eeb9a90a5091d3c9ff082690
James Atherton Kent <james.atherton.kent@gmail.com>
2013-09-12 19:35:10 -0700

    refactored ground to have an array of entity objects, this will make upcoming collision detection easier

--- js/src/ground.js
+++ js/src/ground.js
@@ -5,18 +5,18 @@ function Ground(position, sprite) {
     this.sprite = sprite;
 
     this.positions = new Array(); 
-    _.each(_.range(70), function(x){ this.push(new Vector2d(x*24 , 450)) }, this.positions);    
+    _.each(_.range(70), function(x){ this.push(new Entity2d(new Vector2d(x*24 , 450), sprite)) }, this.positions);    
 }
 
 Ground.prototype = new Entity2d();
 
 Ground.prototype.draw = function() {
-    var drawIt = function(it){ this.sprite.draw(it) }
-    _.each(this.positions, drawIt, this);
-}
-
-Ground.prototype.moveLeft = function() {
-    var moveItLeft = function(it){ it.getX() <= -795 ? it.addX(1595) : it.addX(-5) }
-    _.each(this.positions, moveItLeft);
+    var drawIt = function(it){ it.draw() }
+    _.each(this.positions, drawIt);
 };
 
+Ground.prototype.moveLeft = function() {
+    var moveItLeft = function(it){ it.moveLeft() }
+    _.each(this.positions, moveItLeft);
+};
+ //it.getX() <= -795 ? it.addX(1595) : it.addX(-5) 
